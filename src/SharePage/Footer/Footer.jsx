ba955e976b18289cb983ffb4fadc035c1e594d83
Siam10101 <mehedihasansiam030@gmail.com>
2023-06-16 11:06:38 +0600

    complete a footer section

--- src/SharePage/Footer/Footer.jsx
+++ src/SharePage/Footer/Footer.jsx
@@ -17,10 +17,10 @@ const Footer = () => {
       </div>
       <div>
         <span className="footer-title">Services</span>
-        <a className="link link-hover">Branding</a>
-        <a className="link link-hover">Design</a>
-        <a className="link link-hover">Marketing</a>
-        <a className="link link-hover">Advertisement</a>
+        <a className="link link-hover">Art Courses</a>
+        <a className="link link-hover">Craft Courses</a>
+        <a className="link link-hover">Photography Classes</a>
+        <a className="link link-hover">Online Workshops</a>
       </div>
       <div>
         <span className="footer-title">Company</span>
@@ -30,7 +30,7 @@ const Footer = () => {
         <a className="link link-hover">Press kit</a>
       </div>
       <div>
-        <span className="footer-title">Legal</span>
+        <span className="footer-title">security</span>
         <a className="link link-hover">Terms of use</a>
         <a className="link link-hover">Privacy policy</a>
         <a className="link link-hover">Cookie policy</a>
