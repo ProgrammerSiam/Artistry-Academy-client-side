eb273a878d591e285dc6b7265623337af10480e6
Siam10101 <mehedihasansiam030@gmail.com>
2023-06-17 20:57:53 +0600

    complete a error page

--- src/Pages/ErrorPage/ErrorPage.jsx
+++ src/Pages/ErrorPage/ErrorPage.jsx
@@ -1,30 +1,32 @@
-import React from 'react'
-import Lottie from "lottie-react";
-import errorAnimation from "../../assets/error animation.json";
-import { Link, useLocation, useRouteError } from 'react-router-dom';
-import { Helmet } from 'react-helmet-async';
-
+import React from "react";
+// import Lottie from "lottie-react";
+// import errorAnimation from "../../assets/error animation.json";
+import { Link, useLocation, useRouteError } from "react-router-dom";
+// import { Helmet } from "react-helmet-async";
 
 const ErrorPage = () => {
-    const { error, status } = useRouteError()
-    // console.log(status)
-    return (
-        <div>
-            <div className="flex items-center justify-center ">
-                <div className="w-full md:w-3/4 ">
+  const { error, status } = useRouteError();
+  // console.log(status)
+  return (
+    <div className="flex justify-center items-center h-screen">
+      <div className="  ">
+        {/* <div className="w-full md:w-3/4 ">
                     <Lottie className='h-[600px]' animationData={errorAnimation}></Lottie>
-                </div>
-                <div className="w-full md:w-1/2 space-y-3 ">
-                    <p className="text-3xl font-bold"> <span className=''>Opps!</span>  {status}</p>
-                    <p className="text-error font-bold">{error?.message}</p>
-                    <button >
-                        <Link to='/' className="px-6 py-2 rounded-md bg-error font-bold">Back to Home</Link>
-                    </button>
+                </div> */}
 
-                </div>
-            </div>
+        <div className="">
+          <p className="text-3xl font-bold">
+            <span className="">Opps!</span> {status}
+          </p>
+          <p className="text-error font-bold">{error?.message}</p>
+          <br />
+          <Link to="/" className="px-6 py-2 mt-10 text-black border font-bold">
+            Back to Home
+          </Link>
         </div>
-    )
-}
+      </div>
+    </div>
+  );
+};
 
-export default ErrorPage
+export default ErrorPage;
